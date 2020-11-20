0e03126cb8a9163e9a9c87ed5f71acef62e03412
JonasFrey96 <frey.breitenbrunn@gmx.de>
2020-11-20 17:47:58 +0000

    trajektory update

--- src/state/trajectory.py
+++ src/state/trajectory.py
@@ -7,6 +7,15 @@ class Trajectory():
   def __len__(self):
     return len(self._poses)
 
+  def __str__(self):
+    s = '='*60
+    s += f'\nTrajektory Length {len(self)} \n'
+    if len(self)> 2:
+      s += f'Start:  \n{self._poses[0]} \n'
+      s += f'Stop:  \n{self._poses[-1]} \n'
+    s += '='*60
+    return s
+    
   def append(self,pose):
     self._poses.append(pose)
   
@@ -17,4 +26,15 @@ class Trajectory():
 
   def relative(self, i,j):
     pass
+
+if __name__ == "__main__":
+  from scipy.stats import special_ortho_group
+  tra = Trajectory()
+  for i in range(0,10):
+    h = np.eye(4)
+    h[:3,:3] = special_ortho_group.rvs(3)
+    tra.append( h )
   
+  print(tra)
+      
+
