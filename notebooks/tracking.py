26af1ef00ece9e8170a59c5ade8ff9933912716c
Carter <cafang@ethz.ch>
2020-12-26 19:01:02 -0800

    Tested KLT tracking notebook with SIFT features instead of shi-tomasi

--- notebooks/tracking.py
+++ notebooks/tracking.py
@@ -12,12 +12,12 @@ K = [[331.37, 0.0,       320.0],
 im_paths = sorted(glob(os.path.join(im_dir, '*.png')))
 frame_idx = 0
 detect_interval = 1
-max_tracks = 10
-max_bidir_error = 50
+max_tracks = 20
+max_bidir_error = 30
 
-lk_params = dict( winSize  = (51, 51),
+lk_params = dict( winSize  = (31, 31),
                   maxLevel = 3,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 50, 0.03))
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.3,
@@ -27,7 +27,7 @@ feature_params = dict( maxCorners = 100,
 tracks = []
 while frame_idx < len(im_paths):
 
-    curr_frame = cv2.imread(im_paths[frame_idx], cv2.IMREAD_COLOR)
+    curr_frame = cv2.imread(im_paths[frame_idx], cv2.IMREAD_GRAYSCALE)
     # curr_frame = cv2.blur(curr_frame, (3, 3))
     curr_frame = cv2.bilateralFilter(curr_frame, 3, 1, 1)
     vis = curr_frame.copy()
@@ -59,14 +59,20 @@ while frame_idx < len(im_paths):
         cv2.waitKey(0)
 
     print(f"Tracked points: {len(tracks)}")
-    curr_frame_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
+    # curr_frame_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
+    curr_frame_gray = curr_frame.copy()
     if frame_idx % detect_interval == 0:
         mask = np.zeros_like(curr_frame_gray)
         mask[:] = 255
         for x, y in [np.int32(tr[-1]) for tr in tracks]:
             cv2.circle(mask, (x, y), 5, 0, -1)
 
-        p = cv2.goodFeaturesToTrack(curr_frame_gray, mask=mask, **feature_params)
+        # p = cv2.goodFeaturesToTrack(curr_frame_gray, mask=mask, **feature_params)
+        # cv_p = cv2.KeyPoint_convert(p)
+        sift = cv2.SIFT_create()
+        cv_p = sift.detect(curr_frame_gray, mask=mask)
+        cv_p, d = sift.compute(curr_frame_gray, cv_p)
+        p = cv2.KeyPoint_convert(cv_p)
         if p is not None:
             for x, y in np.float32(p).reshape(-1, 2):
                 tracks.append([(x, y)])
